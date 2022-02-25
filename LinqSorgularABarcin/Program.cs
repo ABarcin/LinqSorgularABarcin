6f14d0ba8579f1c6f8e7c5dd5be40d1c3dab22d6
Ahmet Barçın <ahmet_barcin@hotmail.com>
2022-02-25 14:44:54 +0300

    modified

--- LinqSorgularABarcin/Program.cs
+++ LinqSorgularABarcin/Program.cs
@@ -10,8 +10,37 @@ namespace LinqSorgularABarcin
     {
         static void Main(string[] args)
         {
-            
+            Slytherin_AracIhaleEntities context = new Slytherin_AracIhaleEntities();
+            int year = 2019;
+            int year2 = 2023;
+            //Belirtilen iki tarih arasında 2.el teklif verilen araç sayısı ve Marka Model bilgisini sıralayan ihale çıkış fiyatının ustunde olanların ortalamasını alıp ortalama fiyatın ustunde değer verenlerin "ortalamanın ustunde" , ortalama fiyatın altında teklif verenlerin ise "ortalamanın altında" olarak belirteln bir rapor çekiniz. 
+            var query = (from aracIhale in context.IhaleArac.ToList()
+                         join aracTeklif in context.AracTeklif.ToList() on aracIhale.IhaleAracID equals aracTeklif.IhaleAracID
+                         join arac in context.Arac.ToList() on aracIhale.AracID equals arac.AracID
+                         where aracTeklif.Tarih.Value.Year>year && aracTeklif.Tarih.Value.Year<year2
+                         group new {aracIhale,aracTeklif,arac } by new {arac.AracID } into gr
+                         select new 
+                         {
+                            AracID=gr.Key,
+                            Average=gr.Average(x=>x.aracTeklif.TeklifFiyat),
+                            Price=gr.Where(x=>x.aracTeklif.TeklifOnay==true).Select(x=>x.aracTeklif.TeklifFiyat).FirstOrDefault(),
+                            Model=gr.Select(x=>x.arac.ArabaModel.Ad).FirstOrDefault(),
+                            Marka=gr.Select(x=>x.arac.Marka.Ad),
+                            AracTeklifCount=gr.Select(x=>x.aracIhale.AracID).Count()
+                         }).ToList();
 
+
+            var query2 = (from arac in context.Arac.ToList()
+                                join aracFiyat in context.AracFiyat on arac.AracID equals aracFiyat.AracID
+                                join ia in context.IhaleArac on arac.AracID equals ia.AracID
+                                join t in context.AracTeklif on ia.IhaleAracID equals t.IhaleAracID
+                                group new { arac, aracFiyat, t } by new { arac.AracID } into arc
+                                select new
+                                {
+                                    AracID = arc.Key,
+                                    KarMiktari = (arc.Where(x => x.t.TeklifOnay == true).OrderByDescending(x => x.t.TeklifFiyat).Select(x => x.t.TeklifFiyat).FirstOrDefault()) - (arc.OrderByDescending(x => x.aracFiyat.Tarih)).Select(x => x.aracFiyat.Fiyat).FirstOrDefault()
+                                }).ToList();
+            Console.ReadLine();
 
         }
     }
